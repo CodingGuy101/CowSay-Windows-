fae60232e80a4a5f46e72693f3dec049cdcfcbc4
CodingGuy101 <43613274+CodingGuy101@users.noreply.github.com>
2018-09-26 10:59:12 -0400

    CowSay.bat
    CowSay for Windows

---
+++ CowSay.bat
@@ -0,0 +1,23 @@
+
+@echo off
+cls
+:CS
+echo What do you want the cow to say?
+set /p %inpt% "input=Enter->"
+cls
+goto SC
+:SC
+cls  
+
+echo (%input%)
+echo     \  ()__()
+echo      \  (oo)\_______
+echo         (__)\       )\/\
+echo             ------w 
+echo           //     \\
+
+pause; >nul
+
+
+      
+
